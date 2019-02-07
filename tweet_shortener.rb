ebcf718360a4614d7f82e7daeb08a32d57a3aea3
Nicholas Dover <dovern42@gmail.com>
2019-02-07 04:37:05 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -12,7 +12,7 @@ at: "@",
 and: "&"
 }
 
-def word_substituter(tweet)
+def word_substituter(tweet, dictionary)
   twerray = tweet.split(" ")
   dictionarystring = dictionary.keys.map {|key| key.to_s}
   twerray = twerray.map do |word|
