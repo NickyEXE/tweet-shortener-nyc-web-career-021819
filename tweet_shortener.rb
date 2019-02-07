d2ef111e21b353904fa5e3c47382b9c7cf845ddb
Nicholas Dover <dovern42@gmail.com>
2019-02-07 04:37:15 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,5 @@
 # Write your code here.
-dictionary = {
+def dictionary = {
 hello: 'hi',
 to: '2',
 two: '2',
@@ -12,7 +12,7 @@ at: "@",
 and: "&"
 }
 
-def word_substituter(tweet, dictionary)
+def word_substituter(tweet)
   twerray = tweet.split(" ")
   dictionarystring = dictionary.keys.map {|key| key.to_s}
   twerray = twerray.map do |word|
